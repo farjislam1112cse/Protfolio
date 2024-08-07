253bc23cb82de328502c52bde0cbdea17b428a29
Farjana Badhon <farjislam1112@gmail.com>
2024-08-07 22:14:50 +0600

    upd

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -9,8 +9,8 @@ import { FaDribbble, FaGithub, FaLinkedinIn, FaYoutube } from 'react-icons/fa';
 
 const Banner = () => {
     return (
-        <div id='home' className='section flex items-center min-h-[85vh] lg:min-h-[70vh] mt-12'>
-            <div className='container mx-auto flex items-center flex-col lg:flex-row'>
+        <div id='home' className='section flex  min-h-[85vh] lg:min-h-[70vh] '>
+            <div className='container mx-auto flex  flex-col lg:flex-row'>
                 {/* text */}
                 <div className=' w-full lg:w-[60%]'>
                     <div className='flex-1 text-center lg:text-left font-secondary'>
@@ -27,15 +27,15 @@ const Banner = () => {
                             ]}></TypeAnimation>
                         </motion.div>
                     </div>
-                    <motion.p variants={fadeIn('up', 0.5)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='mb-8 max-w-lg mx-auto lg:mx-0 lg:text-left text-center'>
+                    <motion.p variants={fadeIn('up', 0.5)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='mb-6 max-w-lg mx-auto lg:mx-0 lg:text-left text-center text-xl'>
                         Passionate Frontend Developer skilled in creating dynamic, responsive, and user-friendly web interfaces. Proficient in HTML, CSS, JavaScript, React, and Tailwind. Dedicated to crafting seamless digital experiences and bringing designs to life with precision and creativity.
                     </motion.p>
-                    <motion.div variants={fadeIn('up', 0.6)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='flex gap-x-6 max-w-max mx-auto lg:mx-0 mb-12  items-center '>
+                    <motion.div variants={fadeIn('up', 0.6)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='flex gap-x-6 max-w-max mx-auto lg:mx-0 mb-8  items-center '>
                         <button className='btn btn-lg'>contact me</button>
                         <a href='' className='text-gradient btn-link'>my protfolio</a>
                     </motion.div>
                     {/* social */}
-                    <motion.div variants={fadeIn('up', 0.7)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0 py-3'>
+                    <motion.div variants={fadeIn('up', 0.7)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0 py-4 lg:py-0'>
                         <a className='' href='https://github.com/farjislam1112cse?tab=repositories'><FaGithub className='hover:drop-shadow-[0_5px_5px_rgba(255,255,255,0.7)]'></FaGithub></a>
                         <a href='https://www.linkedin.com/in/farjana-badhon-9b91541b1/'><FaLinkedinIn className='hover:drop-shadow-[0_5px_5px_rgba(255,255,255,0.7)]' /></a>
                         <a href=''><FaDribbble className='hover:drop-shadow-[0_5px_5px_rgba(255,255,255,0.7)]'></FaDribbble></a>
@@ -45,7 +45,7 @@ const Banner = () => {
                 {/* image */}
                 <motion.div variants={fadeIn('down', 0.5)} initial="hidden" whileInView={'show'} className=' w-full lg:w-[30%] max-w-max mx-auto lg:mx-0 '>
                    
-                    <img className=' max-w-full rounded-full  bg-gradient-to-r from-[#B809C3] to-[#260129]' src={Image} alt="" />
+                    <img className=' max-w-[80%] mx-auto lg:max-w-full  rounded-full  bg-gradient-to-r from-[#B809C3] to-[#260129]' src={Image} alt="" />
 
                 </motion.div>
             </div>
